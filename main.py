c7372e388df02ca43e447000bf3145cbec0c0af9
Elie10101 <eskaf06@gmail.com>
2024-10-15 13:27:24 -0400

    tree

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 3# Put your function here
-
+def factorial(x):
+    if x == 1:
+        return 1
+    # base case
+    else:
+        return x * (factorial(x-1))
+    # general case
 
 # testing
 num = 5
